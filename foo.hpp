a53d6d946dc7ed79d7c89a081d0256d5f3cfe041
Zumite27 <zakowska.zuzanna@gmail.com>
2024-11-25 23:59:25 +0100

    Update foo.hpp
    Zejsciowka 5

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,12 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> v;
+    v.reserve(people.size());
+
+    for(auto element = people.rbegin(); element != people.rend(); ++element){
+        element->birthday();
+        v.push_back(element->isMonster() ? 'n': 'y');
+    }
+    return v;
 }
